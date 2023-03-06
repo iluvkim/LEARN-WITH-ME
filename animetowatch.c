c844317504eedac33dc8f4e70fd2a8cfec37105a
iluvkim <122483329+iluvkim@users.noreply.github.com>
2023-03-07 01:54:50 +0200

    Create animetowatch.c

---
+++ animetowatch.c
@@ -0,0 +1,150 @@
+#include <stdio.h>
+
+int main() {
+    
+    
+ int number;
+ printf("Enter the number between 1 to 31: ");
+ scanf("%d", number);
+ printf("The days you were born is your anime");
+ 
+ switch(number){
+     
+    case 1:
+    printf("Demon Slayer: Kimetsu no Yaiba");
+    break;
+    
+    case 2:
+    printf("Jujutsu Kaisen");
+    break;
+    
+    case 3:
+    printf("Black Clover");
+    break;
+    
+    
+    case 4:
+    printf("Hunter x Hunter");
+    break;
+    
+    case 5:
+    printf("One-Punch Man");
+    break;
+    
+    case 6:
+    printf("Naruto: Shippuden");
+    break;
+    
+    case 7:
+    printf("Attack on Titan");
+    break;
+    
+    case 9:
+    printf("One Peirce");
+    break;
+    
+   case 10:
+    printf("My Hero Academia");
+    break;
+    
+    case 11:
+    printf("Dr. Stone");
+    break;
+    
+    
+    case 12:
+    printf("Erased");
+    break;
+    
+    case 13:
+    printf("Tokyo Ghoul");
+    break;
+    
+    case 14:
+    printf("Death Note");
+    break;
+    
+    case 15:
+    printf("The Seven Deadly Sins");
+    break;
+    
+    case 16:
+    printf("Code Geass: Lelouch of the Rebellion");
+    break;
+    
+    case 17:
+    printf("Food Wars!: Shokugeki No Soma");
+    break;
+    
+    
+    case 18:
+    printf("Mob Psycho 100");
+    break;
+    
+    case 19:
+    printf("The Disastrous Life of Saiki K.");
+    break;
+    
+    
+    case 20:
+    printf("Haikyu!! ");
+    break;
+    
+    
+    case 21:
+    printf("Bleach");
+    break;
+    
+    
+    case 22:
+    printf("Tokyo Revengers");
+    break;
+    
+    
+    case 23:
+    printf("Parasyte: The Maxim");
+    break;
+    
+    
+    case 24:
+    printf("Your Lie in April");
+    break;
+    
+    
+    case 25:
+    printf("Cowboy Bebop");
+    break;
+    
+    case 26:
+    printf("The Rising of the Shield Hero");
+    break;
+    
+    case 27:
+    printf("Fairy Tail");
+    break;
+    
+    case 28:
+    printf("Kaguya-sama: Love is War");
+    break;
+    
+    case 29:
+    printf("Akame ga Kill!");
+    break;
+    
+    case 30:
+    printf("Spy x Family");
+    break;
+    
+    case 31:
+    printf("Fullmetal Alchemist");
+    break;
+    
+    default:
+    printf("Invalid number");
+    
+ }
+ 
+ printf("Anime to watch");
+    
+    return 0;
+}
